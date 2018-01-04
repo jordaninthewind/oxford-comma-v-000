a1d299f92fcfa6244d10e3c3d4ba92bc5b3544e4
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 22:47:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   return_string = ""
   array.each_with_index do |el, i|
     unless i == array.length - 2
-      return_string << "#{el}, "
+      return_string += "#{el}, "
     end
   end
 end
